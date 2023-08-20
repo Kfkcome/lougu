ab8d3828b7e6e5e2cfea950c9d0806746fce9f56
Kfkcome <killzion666@gmail.com>
2023-08-20 19:18:08 +0800

    2023/08/20:前缀和 差分P3406

--- P3406.cpp
+++ P3406.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N, M;
-int P[1000];
-int way[1003];
+int P[100009];
+long long  way[100009];
 struct tickts
 {
     int a, b, c;
 };
-struct tickts t[1001];
+struct tickts t[100009];
 int main()
 {
     cin >> N >> M;
@@ -19,18 +19,18 @@ int main()
     {
         cin >> t[i].a >> t[i].b >> t[i].c;
     }
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i < M-1; i++)
     {
         int start = min(P[i], P[i + 1]);
         int end = max(P[i], P[i + 1]);
         way[start]++;
         way[end]--;
     }
-    int sum = 0;
-    for (int i = 1; i <= N - 1; i++)
+    unsigned long long sum = 0;
+    for (int i = 1; i <= N-1; i++)
     {
         way[i] = way[i] + way[i - 1];
-        sum += min(way[i] * t[i-1].a, way[i] * t[i-1].b + t[i-1].c);
+        sum += min(way[i] * t[i].a, way[i] * t[i].b + t[i].c);
     }
     cout << sum;
     return 0;
