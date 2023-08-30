8d76a33cf0a1cf0f3742398e637a6efcea6f5bbb
Kfkcome <killzion666@gmail.com>
2023-08-30 20:33:10 +0800

    auto-commit

--- P2196.cpp
+++ P2196.cpp
@@ -1,9 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int N;
-int num[21];
-bool links[21][21];
-int dp[21];
+int num[300];
+bool links[300][300];
+int dp[200];
+int pre[300];
 int main()
 {
     cin>>N;
@@ -13,11 +14,14 @@ int main()
     }
     for(int i=1;i<=N-1;i++)
     {
-        for(int j=2;j<=N-i;j++)
+        for(int j=i+1;j<=N;j++)
         {
             cin>>links[i][j];
             links[j][i]=links[i][j];
         }
+    }
+    for(int i=0;i<=N;i++){
+        pre[i]=i;
     }
     for(int i=1;i<=N;i++){
         int maxi=1;
@@ -27,12 +31,27 @@ int main()
             }
         }
         dp[i]=num[i]+dp[maxi];
+        pre[i]=maxi;
     }
     int maxi=0;
     for(int i=1;i<=N;i++)
     {
-        maxi=max(maxi,dp[i]);
+        if(dp[i]>dp[maxi]){
+            maxi=i;
+        }
     }
-    cout<<maxi;
+    int i=maxi;
+    int *way=new int [N+1];
+    int l=0;
+    way[l++]=maxi;
+    while (pre[i]!=i)
+    {
+        way[l++]=pre[i];
+        i=pre[i];
+    }
+    for(int i=l-1;i>=0;i--){
+        cout<<way[i]<<' ';
+    }
+    cout<<endl<<dp[maxi];
     return 0;
 }
