775557f868e234a85f0a87c2ddfa92de2615db60
Kfkcome <killzion666@gmail.com>
2023-09-13 15:13:32 +0800

    2023/09/13:P1064:动态规划+复杂递推公式

--- P1064.cpp
+++ P1064.cpp
@@ -36,7 +36,7 @@ int main()
     }
     for (int i = 1; i <= m; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = n; j >= 10; j-=10)
         {
             if (!g[i].top)
             {
@@ -47,9 +47,16 @@ int main()
                 dp[j] = max(dp[j], dp[j-g[i].v]+g[i].v*g[i].p);
             }
             if(g[i].q_num>=1&&j>=g[g[i].q[0]].v+g[i].v){
-                dp[j]=max(dp[j],dp[g[g[i].q[0]].v+g[i].v])
+                int q_0=g[i].q[0];
+                dp[j]=max(dp[j],dp[j-g[q_0].v-g[i].v]+g[i].v*g[i].p+g[q_0].v*g[q_0].p);
+            }
+            if(g[i].q_num>=2&&j>=g[g[i].q[0]].v+g[i].v+g[g[i].q[1]].v){
+                int q_0=g[i].q[0];
+                int q_1=g[i].q[1];
+                dp[j]=max(dp[j],dp[j-g[q_0].v-g[q_1].v-g[i].v]+g[i].v*g[i].p+g[q_1].v*g[q_1].p+g[q_0].v*g[q_0].p);
             }
         }
     }
+    cout<<dp[n];
     return 0;
 }
