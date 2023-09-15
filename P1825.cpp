1ed7583d8ad7021e1219e03dad41662915f58b06
Kfkcome <killzion666@gmail.com>
2023-09-15 11:33:11 +0800

    2023:9/15:同步

--- P1825.cpp
+++ P1825.cpp
@@ -1,11 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N, M;
-char place[301][301];
-int start_i, start_j, end_i, end_j;
-bool dfs(int count)
+struct point
 {
+    int i, j;
+};
+char place[301][301];
+map<char, list<struct point> > transit;
+bool path[301][301];
+int min_time;
+int start_i, start_j, end_i, end_j;
+bool dfs(int count, int i, int j)
+{
+    path[i][j] = true;
+    if (place[i][j] == '=')
+    {
+        min_time = min(count, min_time);
+        return true;
+    }
+    if (place[i][j] >= 'A' && place[i][j] <= 'Z')
+    {
 
+        list<struct point>::iterator it1 = transit[place[i][j]].begin();
+        for (; it1 != transit[place[i][j]].end(); it1++)
+        {
+            if ((*it1).i != i || (*it1).j != j)
+                dfs(count + 1, (*it1).i, (*it1).j);
+        }
+        for()
+    }
+    if (!path[i - 1][j])
+        dfs(count + 1, i - 1, j);
 }
 int main()
 {
@@ -25,9 +50,14 @@ int main()
                 end_i = i;
                 end_j = j;
             }
+            if (place[i][j] >= 'A' && place[i][j] <= 'Z')
+            {
+                struct point p = {i, j};
+                transit[place[i][j]].insert(transit[place[i][j]].end(), p);
+            }
         }
     }
-    cout << dfs(0)
+    // cout << dfs(0)
 
-            return 0;
+    return 0;
 }
