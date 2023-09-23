5c79c37b8c97138fa0f81a16dfcba178bdd75a3d
Kfkcome <killzion666@gmail.com>
2023-09-23 14:28:06 +0800

    2023/09/23:P1605 深度优先搜索DFS

---
+++ P1433.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+struct cheese
+{
+    int x, y;
+};
+<<<<<<< HEAD
+struct cheese c[16];
+bool way[16];
+double path[16][16];
+double length_mini = 1 << 29;
+void dfs(int last_point, int i, double path_sum)
+{
+    if (path_sum > length_mini)
+    {
+        return;
+    }
+    if (i == n)
+    {
+        length_mini = min(length_mini, path_sum);
+        return;
+    }
+    for (int k = 1; k <= n; k++)
+    {
+        if (!way[k])
+        {
+            way[k] = true;
+            dfs(k, i + 1, path_sum + path[last_point][k]);
+            way[k] = false;
+        }
+    }
+}
+int main()
+{
+    cin >> n;
+    c[0].x = 0, c[0].y = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> c[i].x >> c[i].y;
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = i; j <= n; j++)
+        {
+            int x1 = c[i].x, y1 = c[i].y;
+            int x2 = c[j].x, y2 = c[j].y;
+            path[i][j] = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            path[j][i] = path[i][j];
+        }
+    }
+    dfs(0, 0, 0);
+    cout << fixed << setprecision(2) << length_mini;
+    return 0;              
+=======
+double path[16][16];
+struct cheese c[16];
+int main()
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c[i].x >> c[i].y;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double x1=c[i].x,x2=c[j].x;
+            double y1=c[i].y,y2=c[j].y;
+            path[i][j]=sqrt()
+        }
+    }
+    return 0;
+>>>>>>> a4f47ae66adb9b5e862d4629e928ef08431126f9
+}
