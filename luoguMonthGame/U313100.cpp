62d8001e242106701372d174791ece487c36e2a7
Kfkcome <killzion666@gmail.com>
2023-10-02 11:08:56 +0800

    2023/10/02:P1596 深度优先搜索

---
+++ luoguMonthGame/U313100.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+struct elec{
+    int pos;
+    bool kind;//真就是正电子
+};
+stack<elec> left,right;//向左的和向右的栈
+string position;
+int main()
+{
+    int n;
+    cin>>n>>position;
+    for(int i=0;i<position.length();i++)
+    {
+        if(position[i]=='+'){
+            
+        }
+    }
+    return 0;
+}
