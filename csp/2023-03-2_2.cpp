aacbc800ce660ae992c53e40d0197a494843b17e
Kfkcome <killzion666@gmail.com>
2024-02-27 18:38:00 +0800

    csp : 2023-03-2

--- csp/2023-03-2_2.cpp
+++ csp/2023-03-2_2.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 int c[100001]; // 差分数组 i代表天数 c[i]真实的值代表减到i天需要的资源数
 // 求出c后再转化为前缀和数组
-int csum[100001];
-
 int main()
 {
     int n, m, k;
@@ -13,7 +11,7 @@ int main()
     {
         int t, c_temp;
         cin >> t >> c_temp;
-        maxt = max(maxt, c_temp);
+        maxt = max(maxt, t);
         // 让0-t的区间内增加c
         c[0] += c_temp;
         c[t + 1] -= c_temp;
@@ -22,15 +20,17 @@ int main()
     {
         c[i] += c[i - 1]; // 求前缀和
     }
-    int minday=1e9;
-    for (int i = maxt + 1; i >= 1; i++)
+    int minday = maxt;
+    for (int i = maxt + 1; i >= 1; i--)
     {
-        if (i <= k)
+        if (m > c[i])
         {
-            cout << k;
-            return 0;
+            m -= c[i];
+            minday = i;
         }
-        
+        else
+            break;
     }
+    cout << max(k, minday - 1);
     return 0;
 }
