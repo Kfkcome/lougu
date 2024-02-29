2563e2c98649b3d4e784ee0189d45051062c7ffc
Kfkcome <killzion666@gmail.com>
2024-02-29 12:06:07 +0800

    csp : 2023-03-3

--- UseMap.cpp
+++ UseMap.cpp
@@ -1,13 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+struct equal
+{
+    int id;
+    int val;
+    const bool operator=(struct equal a)
+    {
+        if (a.id == id && a.val == val)
+            return true;
+        return false;
+    }
+};
+map<struct equal, int> m;
+const bool operator<(struct equal a, struct equal b)
+{
+    return a.id < b.id;
+}
+
 int main()
 {
-    map<int,int> m;
-    int a=1;
-    int key=5;
-    m[key]=a;
-    m[1]=0;
-    map<int,int>::iterator it=m.find(9);
-    cout<<it->first<<' '<<it->second<<endl;
-    return 0;
+    struct equal e, e1, e2;
+    e.id = 1;
+    e.val = 10;
+    e1.id = 1;
+    e.val = 11;
+    m[e] = 10;
+    m[e1] = 11;
+    cout << m[e] << endl;
 }
--- csp/2023-03-3.cpp
+++ csp/2023-03-3.cpp
@@ -4,10 +4,12 @@ int n, m;
 struct dn
 {
     int dn;
-    map<int, int> value;
+    unordered_map<int, int> value;
 };
+unordered_map<int, unordered_map<int, unordered_set<int>>> artt; // 第一个int 是属性  第二个int是属性值 第三个int是对应user的dn
+unordered_map<int, unordered_set<int>> args;
 struct dn user[2501];
-void solveBase(vector<int> &t, string temp)
+void solveBase(unordered_set<int> &t, string temp)
 {
     // 寻找操作operator,并且分解出属性和值
     int id, val, op; // 如果op是1 则符号是：如果op是0 则符号是~
@@ -24,19 +26,18 @@ void solveBase(vector<int> &t, string temp)
             break;
         }
     }
-    for (int i = 0; i < n; i++)
+    if (op)
     {
-        if (op && user[i].value.find(id) != user[i].value.end() && user[i].value[id] == val)
-        {
-            t.push_back(i);
-        }
-        else if (!op && user[i].value.find(id) != user[i].value.end() && user[i].value[id] != val)
-        {
-            t.push_back(i);
-        }
+        t.insert(artt[id][val].begin(), artt[id][val].end());
+    }
+    else
+    {
+        t.insert(args[id].begin(), args[id].end()); // 先把所有有这个属性的dn加入
+        for (auto item : artt[id][val])
+            t.erase(item); // 然后清楚和这个值相等的dn
     }
 }
-void solveExpr(vector<int> &t, string temp)
+void solveExpr(unordered_set<int> &t, string temp)
 {
     int logic; // 如果是 &与 则为0  如果是 |或 则为1
     if (temp[0] == '&')
@@ -50,19 +51,19 @@ void solveExpr(vector<int> &t, string temp)
     }
     int position[4]; // 记录四个括号符号的位置
     position[0] = 1;
-    stack<int> pos;
+    int pos = 0;
     // 匹配括号的位置
     for (int i = 2; i < temp.size(); i++)
     {
         if (temp[i] == '(')
         {
-            pos.push(i);
+            pos++;
         }
         if (temp[i] == ')')
         {
-            if (!pos.empty())
+            if (pos != 0)
             {
-                pos.pop();
+                pos--;
             }
             else
             {
@@ -73,32 +74,29 @@ void solveExpr(vector<int> &t, string temp)
             }
         }
     }
-    vector<int> t1;
+    unordered_set<int> t1;
     solveExpr(t1, string(temp.begin() + position[0] + 1, temp.begin() + position[1]));
-    vector<int> t2;
+    unordered_set<int> t2;
     solveExpr(t2, string(temp.begin() + position[2] + 1, temp.begin() + position[3]));
     if (logic == 0)
     {
-        for (auto item : t1)
+        for (auto item : t2)
         {
-            for (auto item2 : t2)
-            {
-                if (item == item2)
-                {
-                    t.push_back(item);
-                    break;
-                }
-            }
+            if (t1.find(item)!=t1.end())
+                t.insert(item);
         }
     }
     if (logic == 1)
     {
-        t.insert(t.end(), t1.begin(), t1.end());
-        t.insert(t.end(), t2.begin(), t2.end());
+        t.insert(t1.begin(), t1.end());
+        t.insert(t2.begin(), t2.end());
     }
 }
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     cin >> n;
     for (int i = 0; i < n; i++)
     {
@@ -110,21 +108,21 @@ int main()
             int id, val;
             cin >> id >> val;
             user[i].value[id] = val;
+            args[id].insert(user[i].dn);
+            artt[id][val].insert(user[i].dn);
         }
     }
     cin >> m;
     for (int i = 0; i < m; i++)
     {
-        vector<int> ans;
+        unordered_set<int> ans;
         string temp;
         cin >> temp;
         solveExpr(ans, temp);
-        sort(ans.begin(), ans.end()); // 从小到大排序
-        set<int> ans1(ans.begin(), ans.end());
+        vector<int> ans1(ans.begin(), ans.end());
+        sort(ans1.begin(), ans1.end());
         for (auto item : ans1)
-        {
-            cout << user[item].dn << ' ';
-        }
+            cout << item << ' ';
         cout << endl;
     }
     return 0;
