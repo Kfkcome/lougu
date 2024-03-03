268c2ce75d006b7a590510a5f883c6e9bf788327
Kfkcome <killzion666@gmail.com>
2024-03-03 16:50:03 +0800

    csp : 2023-03-3

--- csp/2023-03-4.cpp
+++ csp/2023-03-4.cpp
@@ -1,64 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, q;
-struct betw
+struct range
 {
+    unsigned long long l, r;
     int id;
-    vector<int> l; // 一个int保存16位ip //从高位开始保存
-    vector<int> r;
-};
-
-const bool compareIP(vector<int> a, vector<int> b)
-{
-    int i = 0;
-    for (i = 0; i < min(a.size(), b.size()); i++)
-    {
-        if (a[i] > b[i])
-        {
-            return 1;
-        }
-        else if (a[i] < b[i])
-        {
-            return 0;
-        }
-    }
-    if (a.size() == b.size())
-        return 2; // 两个相等
-    else if (a.size() > b.size())
-        return 1; // a大于b
-    else if (a.size() < b.size())
-        return 0; // a小于b
-}
-// 用来判断a在b的什么位置
-// 1左边  2 左边有交集 3中间  4右边有交集  5 右边  6两个区间相等
-int compareRange(struct betw a, struct betw b)
-{
-    if (compareIP(a.r, b.l) == 0)
-    {
-        return 1;
-    }
-    else if (compareIP(a.r, b.l) == 2 || ((compareIP(a.r, b.l) == 1 || compareIP(a.r, b.l) == 2) && compareIP(a.l, b.l) == 0 && compareIP(a.r, b.r) == 0))
-    {
-        return 2;
-    }
-    else if (compareIP(a.r, b.r) == 0 && compareIP(a.l, b.l) == 1)
-    {
-        return 3;
-    }
-    else if (compareIP(a.r, b.r) == 2 || (compareIP(a.r, b.r) == 1 && (compareIP(a.l, b.l) == 1 || compareIP(a.l, b.l) == 2) && compareIP(a.l, b.r) == 0))
-    {
-        return 4;
-    }
-    else if (compareIP(a.l, b.r) == 1)
-    {
-        return 5;
-    }
-    else if (compareIP(a.l, b.l) == 2 && compareIP(a.r, b.r) == 2)
-        return 6;
-}
-vector<struct betw> user;
+} user[20001];
 int top;
-int hexToDec(string a)
+unsigned long long hexToDec(string a) // 十六进制字符串转整形
 {
     int result = 0;
     for (int i = 0; i < a.size(); i++)
@@ -75,85 +24,144 @@ int hexToDec(string a)
     }
     return result;
 }
-struct betw convertString(string l, string r, int id)
+unsigned long long convertString(string t)
 {
-    int lastpos = -1; // 记录上一个冒号的位置
-    struct betw res;
-    int top = 0; // 记录当前的位数
-    for (int i = 0; i < l.size(); i++)
+    int last = 0;
+    int result = 0;
+    for (int i = 0; i < n / 16; i++)
     {
-        if (l[i] == ':')
-        {
-            res.l.push_back(hexToDec(string(l.begin() + lastpos + 1, l.begin() + 1)));
-        }
+        result *= 65536;
+        result += hexToDec(string(t.begin() + i * 5, t.begin() + i * 5 + 4));
     }
-    for (int i = 0; i < r.size(); i++)
-    {
-        if (r[i] == ':')
-        {
-            res.r.push_back(hexToDec(string(r.begin() + lastpos + 1, r.begin() + 1)));
-        }
-    }
-    res.id = id;
-    return res;
+    return result;
 }
-bool check(struct betw t) // 检查是否可以占用
+bool allocate(struct range t)
 {
-    bool flag = true;
-    int insertTo = -1;
-    // 怎么保证区间之间有交集
-    for (int i = 0; i <= user.size(); i++) // 循环遍历每一个范围查看是不是有冲突
+    struct range temp = t;
+    for (int i = 0; i < top; i++)
     {
-        int kind = compareRange(t, user[i]);
-
-        if (user[i].id == t.id) // 如果是同一个用户 只要不是全部分配了就可以分配
+        if (user[i].id == t.id)
         {
-            if (kind == 6) // 如果已经全部被分配了
+            // 如果两个有交集
+            if (!(user[i].l > t.r) && !(user[i].r < t.l))
             {
-                flag = false;
-            }
-            else if (kind >= 2 && kind <= 5)
-            {
-                insertTo = i;
+                if (t.l >= user[i].l && t.r <= user[i].r) // 如果全部已经被分配了
+                {
+                    return 0;
+                }
+                else
+                {
+                    // 减去他们交集的地方
+                    if (t.l >= user[i].l && t.l <= user[i].r)
+                    {
+                        t.l = user[i].r + 1;
+                    }
+                    if (t.r >= user[i].l && t.r <= user[i].r)
+                    {
+                        t.r = user[i].l - 1;
+                    }
+                }
             }
         }
         else
         {
-            if (kind != 1 || kind != 5)
+            // 如果两个有交集
+            if (!(user[i].l > t.r) && !(user[i].r < t.l))
             {
-                flag = false;
+                return 0;
             }
         }
     }
-    if (!flag)
-        return false;
-    compareRange(t, user[insertTo]);
-}
-int main()
-{
-    int type;
-    cin >> n >> q;
-    for (int i = 0; i < q; i++)
+    // 如果循环下来整个已经被减去
+    // 那么久无法插入，说明全部被都被占用了。
+    if (t.r < t.l)
     {
-        cin >> type;
-        if (type == 1)
+        return 0;
+    }
+    else
+    {
+        user[top++] = temp;
+        return 1;
+    }
+}
+int searchIp(long long ip) // 如果没有找到则返回0
+{
+    for (int i = 0; i < top; i++)
+    {
+        if (user[i].l <= ip && user[i].r >= ip)
         {
-            int id;
-            string l, r;
-            cin >> id >> l >> r;
-            struct betw t = convertString(l, r, id);
-            if (check(t))
+            return user[i].id;
+        }
+    }
+    return 0;
+}
+int searchAll(struct range t)
+{
+    t.id = -1;
+    for (int i = 0; i < top; i++)
+    {
+        // 如果两个有交集
+        if (!(user[i].l > t.r) && !(user[i].r < t.l))
+        {
+            if (t.id != user[i].id && t.id != -1)
             {
+                return 0;
             }
             else
             {
+                t.id = user[i].id;
+                // 减去他们交集的地方
+                if (t.l >= user[i].l && t.l <= user[i].r)
+                {
+                    t.l = user[i].r + 1;
+                }
+                if (t.r >= user[i].l && t.r <= user[i].r)
+                {
+                    t.r = user[i].l - 1;
+                }
             }
         }
-        else if (type == 2)
+    }
+    if (t.l > t.r)
+        return t.id;
+    else
+        return 0;
+}
+int main()
+{
+    cin >> n >> q;
+    for (int i = 0; i < q; i++)
+    {
+        int temp;
+        cin >> temp;
+        if (temp == 1)
         {
+            struct range u;
+            string l, r;
+            cin >> u.id >> l >> r;
+            u.l = convertString(l);
+            u.r = convertString(r);
+            if (allocate(u))
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
         }
-        else if (type == 3)
+        else if (temp == 2)
         {
+            unsigned long long s; // 注意要字符串转换
+            string ss;
+            cin >> ss;
+            s = convertString(ss);
+            cout << searchIp(s) << endl;
+        }
+        else if (temp == 3)
+        {
+            string l, r;
+            cin >> l >> r;
+            struct range u;
+            u.l = convertString(l);
+            u.r = convertString(r);
+            cout << searchAll(u) << endl;
         }
     }
     return 0;
