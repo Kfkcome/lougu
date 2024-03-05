6ae5de38e863f4e5434fe53756ac1a02e164556d
Kfkcome <killzion666@gmail.com>
2024-03-05 14:07:52 +0800

    同步

--- csp/2022-12-4.cpp
+++ csp/2022-12-4.cpp
@@ -8,32 +8,28 @@ struct worker
     int p;  // 上级
 } w[3001];
 
-vector<struct worker> child[3001]; // 使用有序集合进行二分
-int getMin(int i, struct worker t) // 从i的子节点中（包括i）找距离t最近的节点
+vector<struct worker> child[3001];  // 保存所有子节点（包括子节点的子节点）
+vector<struct worker> child1[3001]; // 保存第一层子节点
+void getChild(int i, int j)         // 父节点是i 子节点是j  要把j的所有子节点都填入到i的child数组中
 {
-    if (child[i].size() == 1) // 如果是叶子节点
+    if (child1[j].size() == 1)
     {
-        return abs(child[i][0].a - t.a);
+        child[i].push_back(child1[j][0]);
+        return;
     }
-    int mini = -1;
-    for (auto item : child[i])
+    for (auto item : child1[j])
     {
-        if (item.id != t.id)
+        if (item.id == j)
         {
-            int dis;
-            if (item.id != i) /// 如果不是他本身
-                dis = getMin(item.id, t);
-            else
-                dis = abs(item.a - t.a);
-            if (dis < mini || mini == -1)
-            {
-                mini = dis;
-            }
+            child[i].push_back(item);
         }
+        else
+            getChild(i, item.id);
     }
-    if (mini == -1)
-        mini = 0;
-    return mini;
+}
+bool cmp(struct worker a, struct worker b)
+{
+    return a.a < b.a;
 }
 int main()
 {
@@ -46,16 +42,41 @@ int main()
     {
         cin >> w[i].a;
         w[i].id = i;
-        child[i].push_back(w[i]);      // 自己是自己的员工
-        child[w[i].p].push_back(w[i]); // 自己是上级的员工
+        child1[i].push_back(w[i]);      // 自己是自己的员工
+        child1[w[i].p].push_back(w[i]); // 自己是上级的员工
     }
     for (int i = 1; i <= n; i++)
     {
-        int anx = 0;               // 求和
-        for (auto item : child[i]) // 获取每一项
+        for (auto item : child1[i])
         {
-            int min1 = getMin(i, item);
-            anx += min1 * min1;
+            if (item.id == i)
+            {
+                child[i].push_back(item);
+            }
+            else
+                getChild(i, item.id);
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        sort(child[i].begin(), child[i].end(), cmp);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        int anx = 0; // 求和
+        for (int j = 0; j < child[i].size(); j++)
+        {
+            int l, r, mini = 1e9;
+            if (j != 0)
+            {
+                mini = min(abs(child[i][j].a - child[i][j - 1].a), mini);
+            }
+            if ((j + 1) != child[i].size())
+            {
+                mini = min(abs(child[i][j].a - child[i][j + 1].a), mini);
+            }
+            anx += mini * mini;
         }
         cout << anx << endl;
     }
