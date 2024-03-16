d64be37b468ddd01317e544bbd6c9929ed02287a
Kfkcome <killzion666@gmail.com>
2024-03-16 12:26:09 +0800

    csp : 2021-09-2

--- csp/2021-09-2-2.cpp
+++ csp/2021-09-2-2.cpp
@@ -1,43 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, a[500001], p[10001];
+// 定义p[i]是不超过i的非零段的个数
+// 那么p[i]-p[i-1]的含义是在i-1为0的基础上增加i为0后非零段的个数的增加
 int main()
 {
     cin >> n;
     int max_a = 0;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin >> a[i]; // 输入数据
         max_a = max(max_a, a[i]);
     }
-    // 从a更新p 如果a[i+1]>a[i]
-    // 那么p[a[i+1]]-1到p[a[i]]这个区间内都可以增加一个非零段
-    // 不用判断是不是区间是不是减少
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 1; i <= n; i++)
     {
-        if (a[i + 1] < a[i])
+        if (a[i] > a[i - 1])//如果前一个大于后一个
         {
-            p[a[i + 1]]++;
-            p[a[i]]--;
-        }
-        if (a[i + 1] > a[i])
-        {
-            
-        }
-        if (i == n - 2) // 特判倒数第二个
-        {
-            if (a[i + 1] > a[i])
-            {
-                p[a[i + 1]]--;
-                p[a[i]]++;
-            }
+            p[a[i - 1] + 1]++;
+            p[a[i] + 1]--;
         }
     }
     int max_p = 0;
-    for (int i = 0; i < max_a + 1; i++)
+    for (int i = 1; i <= max_a + 1; i++)
     {
-        if (i > 0)
-            p[i] += p[i - 1];
+        p[i] += p[i - 1];
         max_p = max(max_p, p[i]);
     }
     cout << max_p;
